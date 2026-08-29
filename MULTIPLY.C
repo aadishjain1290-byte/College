17cbbc0bee893511ca56c6bad9239fd5b9a01a17
aadishjain1290-byte <aadishjain1290@gmail.com>
2026-08-29 11:31:09 +0530

    Add files via upload

---
+++ MULTIPLY.C
@@ -0,0 +1,14 @@
+# include <stdio.h>
+
+int main()
+{
+	int num1;
+	int num2;
+	printf("Enter the first number: ");
+	scanf("%d", &num1);
+	printf("Enter the second number: ");
+	scanf("%d", &num2);
+	printf("Multiplication of %d and %d is: %d \n", num1, num2, num1*num2);
+
+	return 0;
+}
