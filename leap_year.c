5b0d7e71866e97009bde16e669daf06edea9d64f
aadishjain1290-byte <aadishjain1290@gmail.com>
2026-09-05 18:57:44 +0530

    Modify leap year program to accept date input
    Updated input to accept day, month, and year.

--- leap_year.c
+++ leap_year.c
@@ -2,9 +2,9 @@
 
 int main()
 {
-    int year;
+    int day,month,year;
     printf("Please enter the year: ");
-    scanf("%d", &year);
+    scanf("%d %d %d", &day, &month, &year);
     if (year%4 == 0 && year%100 !=0)
     {
         printf("%d is a leap year", year);
